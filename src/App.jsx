f0cf7a8fe289374aa33cbd36285431cdd2ec76c0
unknown <braynnermarques@hotmail.com>
2023-06-29 19:54:19 -0300

    finalizando estruturação do projeto

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import "./styles/components/app.sass"
+
+function App() {
+  return <section className="container">
+    <div className="top-side">
+
+      <div className="title">
+        <h1></h1>
+        <p></p>
+      </div>
+
+      <div className="rating">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+    </div>
+
+    <div className="bottom-side">
+
+      <span></span>
+      <span></span>
+      <span></span>
+
+    </div>
+
+  </section>
+      
+}
+
+export default App
